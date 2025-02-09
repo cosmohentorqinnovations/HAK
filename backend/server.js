a665b163bd4f7334004f60067ef430afa639278d
cosmohentorqinnovations <cosmohentorq@gmail.com>
2025-02-09 17:35:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("HAK Cloud API is running!");
+  res.send("HAK Cloud Backend Running!");
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
